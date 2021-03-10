012136e583242acabac4aafa41a136f7738eaa63
Paulina Babiarz <paulina.babiarz@proteansoftware.co.uk>
2021-03-10 15:22:22 +0100

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -5,13 +5,44 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         
 
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                if (averageGrade >= 80)
+                {
+                    return 'A';
+                }
+                else if (averageGrade >= 60)
+                {
+                    return 'B';
+                }
+                else if (averageGrade >= 40)
+                {
+                    return 'C';
+                }
+                else if (averageGrade >= 20)
+                {
+                    return 'D';
+                }
+                else
+                {
+                    return 'F';
+                }
+            }
         }
     }
 }
